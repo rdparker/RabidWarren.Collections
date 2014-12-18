65e89e77bce51735732c56b94f0a295430a9bc6e
Ron Parker <rdparker@gmail.com>
2014-12-17 21:57:03 -0500

    Implement a ToMultimap extension method
    This was not strictly needed, but it was written in the process of adding bound property converters. It may be useful later.

---
+++ Enumerable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RabidWarren.Collections.Generic
+{
+    public static class Enumerable
+    {
+        public static Multimap<TKey, TElement> ToMultimap<TSource, TKey, TElement>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            Func<TSource, TElement> elementSelector)
+        {
+            var map = new Multimap<TKey, TElement>();
+
+            foreach (var entry in source)
+            {
+                map.Add(keySelector(entry), elementSelector(entry));
+            }
+
+            return map;
+        }
+    }
+}
